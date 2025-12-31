f24684663db80138ddaf6754b8f880a7521a3fd6
mohdminhaj12w33-dot <mohdminhaj12w33@gmail.com>
2025-12-31 21:52:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-const inputs = document.querySelectorAll('.code');
+const inputs = document.querySelectorAll(".code");
 
 inputs[0].focus();
 
 inputs.forEach((input, index) => {
 
-  input.addEventListener('input', () => {
+  input.addEventListener("input", () => {
     if (input.value && index < inputs.length - 1) {
       inputs[index + 1].focus();
     }
   });
 
-  input.addEventListener('keydown', (e) => {
-    if (e.key === 'Backspace') {
-      if (input.value === '' && index > 0) {
-        inputs[index - 1].value = '';
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      if (input.value === "" && index > 0) {
         inputs[index - 1].focus();
       } else {
-        input.value = '';
+        input.value = "";
       }
     }
   });
